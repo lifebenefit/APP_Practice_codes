9ea5e58e12fad2f9f254ce23c046a66c9c1120bf
liam <wonhyuk1994@gmail.com>
2024-09-09 18:01:28 +0900

    메인 헤더 추가하기

---
+++ src/shared/components/Navigation/MainNavigation.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+import MainHeader from "./MainHeader";
+import './MainHeader.css'
+
+
+
+
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,16 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-    return <h2>User Works!</h2>
+    const USERS = [{
+        id: "u1",
+        name : 'Max schwartz',
+        image : 'https://image.fmkorea.com/files/attach/new/20171104/486616/155548/825707403/c1d17e9b6d79bb902a331d6a3dd622cc.jpeg',
+        places : 3
+    }];
+
+    return <UsersList items={USERS} />;
+    // return <h2>User Works!</h2>
 };
 
 export default Users;
