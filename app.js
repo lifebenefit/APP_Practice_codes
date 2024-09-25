6e93766ee57c029302b2f947ad7e60a1b240011b
liam <wonhyuk1994@gmail.com>
2024-09-25 19:16:46 +0900

    중단

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ app.use((req, res, next) => {
     '<form method="POST"><input type="text" name="username"><button type="submit"> Create User </button></form>'
   );
   // next();
-  // 안되겠다 시발 이거 다시 듣자
+  // 안되겠다 시발 이거 다시 듣자 
+  //TODO: Express 관련 함수나 구조 파악 필요
 });
 
 app.listen(5000);
